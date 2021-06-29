49ff72e0666c44232633a7524342049c0683cd37
Alice <alice@gamial.com>
2021-06-29 01:20:29 -0300

    Access data layer

---
+++ Books.API/Books.API/Services/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using Books.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Books.API.Services
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetBooksAsync();
+
+        Task<Book> GetBookAsync(Guid id);
+    }
+}
